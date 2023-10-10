207491d3da8ffcccae60405c768b99d798dd3b0a
banalarevanthreddy <81283706+banalarevanthreddy@users.noreply.github.com>
2023-10-09 21:18:45 -0700

    Added project1.py

---
+++ project1.py
@@ -0,0 +1,17 @@
+#String concatenation
+
+"""
+programmer=input("enter the name")
+print(programmer+"is very good in python")
+print("{} is very good in python".format(programmer))
+print(f"{programmer} is very good in python")
+
+"""
+noun=input("enter the noun=")
+adj=input("enter the adj")
+verb=input("enter verb")
+conjunction=input("enter the conjunction")
+
+madlib=f"Once upon a time, there was a {adj} {noun}. This {noun} \
+    loved to {verb} {conjunction} have adventures!"
+print(madlib)
